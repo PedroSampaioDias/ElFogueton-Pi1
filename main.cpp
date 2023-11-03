0775c3f449ba3ec6296810b9faf5681821d0c967
Lucas <lucas.fn4@gmail.com>
2023-11-03 17:28:39 -0300

    Refatoração

--- main.cpp
+++ main.cpp
@@ -3,39 +3,57 @@
 
 #define GPS_SERIAL Serial2
 
+#define BAUD_RATE 9600
+
 TinyGPSPlus gps;
 
-void setup() {
-  GPS_SERIAL.begin(9600);
+struct dados {
+    float latitude;
+    float longitude;
+    float altitude;
+    float velocidade;
+};
 
-  Serial.begin(9600);
+void setup() {
+  GPS_SERIAL.begin(BAUD_RATE);
+  Serial.begin(BAUD_RATE);
+}
+
+void salvar_dados(dados& d) {
+    d.altitude = gps.altitude.meters();
+    d.velocidade = gps.speed.kmph();
+    d.longitude = gps.location.lng();
+    d.latitude = gps.location.lat();
+}
+
+void ler_dados(dados& d) {
+    Serial.println("------------------------------");
+    Serial.print("Latitude: ");
+    Serial.println(d.latitude, 4);
+    Serial.print("Longitude: ");
+    Serial.println(d.longitude, 4);
+    Serial.print("Altitude: ");
+    Serial.println(d.altitude, 4);
+    Serial.print("Velocidade: ");
+    Serial.println(d.velocidade, 4);
+    Serial.print("Direcao: ");
+    Serial.println(gps.course.deg());
+    //Serial.println("Data e Hora: ");
+    //Serial.println(gps.date);
+    Serial.println("------------------------------");
 }
 
 void loop() {
-  while (GPS_SERIAL.available() > 0) {
-    gps.encode(GPS_SERIAL.read());
-  }
+    while (GPS_SERIAL.available() > 0) {
+      gps.encode(GPS_SERIAL.read());
+    }
 
-  if (gps.location.isValid()) {
-    Serial.println("------------------------------");
-    Serial.print("Satelites: ");
-    Serial.println(gps.satellites.value());
-    Serial.print("Latitude: ");
-    Serial.println(gps.location.lat(), 4);
-    Serial.print("Longitude: ");
-    Serial.println(gps.location.lng(), 4);
-    Serial.print("Altitude: ");
-    Serial.println(gps.altitude.meters(), 4);
-    Serial.print("Velocidade: ");
-    Serial.println(gps.speed.kmph(), 4);
-    Serial.print("Direcao: ");
-    Serial.println(gps.course.deg());
-    Serial.println("Data e Hora: ");
-    //Serial.println(gps.date);
-    Serial.println("------------------------------");
+    if (gps.location.isValid()) {
+      salvar_dados(d);
 
+      // Aguarde um segundo antes de atualizar novamente
+      delay(1000);
 
-    // Aguarde um segundo antes de atualizar novamente
-    delay(1000);
-  }
+      ler_dados(d);
+    }
 }
