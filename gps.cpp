3c7ccd03ba614c90a6d433e40dab757db33cf799
Lucas <lucas.fn4@gmail.com>
2023-11-27 19:41:06 -0300

    Pequena refatoração no módulo de GPS

--- gps.cpp
+++ gps.cpp
@@ -5,40 +5,40 @@
 #define GPS_SERIAL Serial2
 #define BAUD_RATE 9600
 
-#define DEBUG 1
+#define DEBUG 0
 
 TinyGPSPlus gps;
 
-DadosInstantaneos dadosInstantaneos = { {0.0, 0.0, 0.0}, 0.0, "00/00/0000", "00:00:00" };
+//DadosInstantaneos dadosInstantaneos = { {0.0, 0.0, 0.0}, 0.0, "00/00/0000", "00:00:00" };
 
 void setup() {
   GPS_SERIAL.begin(BAUD_RATE);
   Serial.begin(BAUD_RATE);
 }
 
-void setDadosInstantaneos() {
+void setDadosInstantaneos(DadosInstantaneos &dadosInstantaneos) {
   if (gps.location.isValid() && gps.altitude.isValid() && gps.speed.isValid() && gps.date.isValid() && gps.time.isValid()) {
     dadosInstantaneos.coordenadas.latitude = gps.location.lat();
     dadosInstantaneos.coordenadas.longitude = gps.location.lng();
     dadosInstantaneos.coordenadas.altitude = gps.altitude.meters();
     dadosInstantaneos.velocidade = gps.speed.mps();
-    setData();
-    setHora();
+    setData(dadosInstantaneos);
+    setHora(dadosInstantaneos);
   } else {
     if(DEBUG)
       Serial.println("DADOS INVALIDOS");
   }
 }
 
-void setData() {
+void setData(DadosInstantaneos &dadosInstantaneos) {
   snprintf(dadosInstantaneos.data, sizeof(dadosInstantaneos.data), "%02d/%02d/%04d", gps.date.day(), gps.date.month(), gps.date.year());
 }
 
-void setHora() {
+void setHora(DadosInstantaneos &dadosInstantaneos) {
   snprintf(dadosInstantaneos.hora, sizeof(dadosInstantaneos.hora), "%02d:%02d:%02d", gps.time.hour(), gps.time.minute(), gps.time.second());
 }
 
-void debugDadosInstantaneos() {
+void debugDadosInstantaneos(DadosInstantaneos &dadosInstantaneos) {
   Serial.println("------------------------------");
   Serial.print("Quantidade de satélites: ");
   Serial.println(gps.satellites.value());
--- gps.h
+++ gps.h
@@ -22,21 +22,25 @@ typedef struct DadosInstantaneos {
 
 /**
  * Salvar os dados de interesse em uma struct.
+ * @params DadosInstantaneos &dadosInstaneos - A struct em questão.
  */
-void setDadosInstantaneos();
+void setDadosInstantaneos(DadosInstantaneos &dadosInstantaneos);
 
 /**
  * Formatar a data e salvar numa struct.
+ * @params DadosInstantaneos &dadosInstaneos - A struct em questão.
  */
-void setData();
+void setData(DadosInstantaneos &dadosInstantaneos);
 
 /**
  * Formatar a hora e salvar numa struct.
+ * @params DadosInstantaneos &dadosInstaneos - A struct em questão.
  */
-void setHora();
+void setHora(DadosInstantaneos &dadosInstantaneos);
 
 /**
  * Printar os dados instantâneos do gps.
+ * @params DadosInstantaneos &dadosInstaneos - A struct em questão.
  */
-void debugDadosInstantaneos();
+void debugDadosInstantaneos(DadosInstantaneos &dadosInstantaneos);
 #endif
