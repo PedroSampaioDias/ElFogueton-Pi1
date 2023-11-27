eb1228b10bd72151ed5d206fb68b985960b97ebb
PedroSampaioDias <pedro.sampaio.dias.rocha@gmail.com>
2023-11-27 09:11:41 -0300

    novo código server

--- server.cpp
+++ server.cpp
@@ -1,31 +1,58 @@
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
-#include <Arduino.h>
+#include <SD.h>
 
-const char* ssid = "ElFogueton";
-const char* password = "elfogueton";
+const char *ssid = "ElFogueton";
+const char *password = "elfogueton";
+
+// Configure o endereço IP estático, gateway e máscara de sub-rede
+IPAddress staticIP(192, 168, 0, 100);
+IPAddress gateway(192, 168, 0, 1);
+IPAddress subnet(255, 255, 255, 0);
 
 AsyncWebServer server(80);
 
+bool dataLoggerAtivo = false;
+
 void setup() {
-  Serial.begin(9660);
-  randomSeed(analogRead(0));
+  Serial.begin(115200);
 
-  // Configurar o ESP32 como ponto de acesso
-  WiFi.mode(WIFI_AP);
+  if (!SD.begin(5)) {
+    Serial.println("Failed to mount SD card");
+    return;
+  }
+
+  // Configure o ponto de acesso com endereço IP estático
+  WiFi.softAPConfig(staticIP, gateway, subnet);
   WiFi.softAP(ssid, password);
+  Serial.print("Endereço IP do Ponto de Acesso: ");
+  Serial.println(WiFi.softAPIP());
 
-  // Rota para a página "Hello World"
-  server.on("/data", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/html", String(random(0, 100)));
+  server.on("/iniciar_gravacao", HTTP_GET, [](AsyncWebServerRequest *request){
+    dataLoggerAtivo = true;
+    request->send(200, "text/plain", "Iniciando o Data Logger\n");
   });
 
-  // Iniciar o servidor
-  server.begin();
+  server.on("/parar_gravacao", HTTP_GET, [](AsyncWebServerRequest *request){
+    dataLoggerAtivo = false;
+    request->send(200, "text/plain", "Parando o Data Logger\n");
+  });
 
-  Serial.println("Endereço IP do Ponto de Acesso: " + WiFi.softAPIP().toString());
+  server.on("/status_gravacao", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/plain", dataLoggerAtivo ? "Ativo\n" : "Inativo\n");
+  });
+
+  server.on("/obter_dados", HTTP_GET, [](AsyncWebServerRequest *request){
+    File arquivo = SD.open("/Dados.txt", "r");
+    if (arquivo && !dataLoggerAtivo) request->send(200, "text/plain", arquivo.readString());
+    else request->send(404, "text/plain", "Erro: Arquivo não existe ou esta sendo escrito\n");
+    arquivo.close();
+  });
+
+  server.begin();
 }
 
 void loop() {
-  // Lidar com eventos assíncronos, se necessário
+  //if(dataLoggerAtivo) Serial.println("Data Logger ativo");
+  //else Serial.println("Data Logger Inativo");
 }
