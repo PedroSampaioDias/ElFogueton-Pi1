4d689bc1a987fe79d1647de46ea3394219cd61dc
Lucas <lucas.fn4@gmail.com>
2023-12-08 18:36:05 -0300

    Alteração de script de início de gravação

--- iniciar_gravacao.sh
+++ iniciar_gravacao.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-curl http://192.168.0.100:80/iniciar_gravacao
+
+if [[ "$1" =~ ^[0-9]+$ || "$number" =~ ^[-][0-9]+$  ]]
+then
+    requisicao="curl http://192.168.0.100:80/iniciar_gravacao?idLancamento=$1"
+
+    echo "fazendo a seguinte requisicao: $requisicao"
+    $requisicao
+else
+    echo "O argumento enviado não é um número inteiro. Finalizando script."
+fi
