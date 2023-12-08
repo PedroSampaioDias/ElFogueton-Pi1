4fddbac22e3662db79ea21a028d9b42030b4dadf
Lucas <lucas.fn4@gmail.com>
2023-12-08 20:20:23 -0300

    Mudança na lógica de salvamento de dados.
    Agora, cada lançamento será salvo num arquivo próprio.

--- iniciar_gravacao.sh
+++ iniciar_gravacao.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-if [[ "$1" =~ ^[0-9]+$ || "$number" =~ ^[-][0-9]+$  ]]
+# verificar se o valor passado por linha de comando é um número inteiro.
+if [[ "$1" =~ ^[0-9]+$ || "$1" =~ ^[-][0-9]+$  ]]
 then
-    requisicao="curl http://192.168.0.100:80/iniciar_gravacao?idLancamento=$1"
+    # indicar qual o nome do arquivo aonde os dados serão salvos.
+    # O nome do arquivo se encontra na primeira linha do arquivo "filepointer"
 
-    echo "fazendo a seguinte requisicao: $requisicao"
-    $requisicao
+    nomeArquivo="retorno_lancamento_$1.csv"
+    echo "$nomeArquivo" > filepointer
+    echo "Os dados serão salvos no arquivo $nomeArquivo."
+
+    # fazer uma requisicao para o endpoint de início de gravação
+    curl http://192.168.0.100:80/iniciar_gravacao
 else
     echo "O argumento enviado não é um número inteiro. Finalizando script."
+    echo "Forma correta de uso: bash inicar_gravacao.sh <numero da gravação>"
+    echo "Exemplo: bash iniciar_gravacao.sh 1"
 fi
--- obter_dados.sh
+++ obter_dados.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
 echo "Iniciando script de obtenção de dados"
+# obter o nome do arquivo em que serão guardados os dados. Esse arquivo é populado ao chamar o script
+# "inicar_gravacao.sh"
+arquivoRetorno=$(head -n 1 filepointer)
+
+echo "Escrevendo os dados no arquivo $arquivoRetorno"
 
 # Loop até receber "EOF"
 while true; do
     # Faz a requisição HTTP
     resposta=$(curl -s http://192.168.0.100:80/obter_dados)
-    
     # Verifica se a resposta é "EOF\n"
     if [ "$resposta" == "EOF" ]; then
         echo "Recebeu a string 'EOF', encerrando o script."
         break
+    # verificar se a ESP32 retornou alguma mensagem de erro e retorná-lo.
     elif [[ "$resposta" == *"Erro"* ]]; then
         echo "$resposta"
         break
+    # popular o arquivo de resposta e printar no terminal
     else
+        echo "$resposta" >> $arquivoRetorno
         echo "$resposta" >> retorno.csv
         echo "$resposta"
     fi
-    
-    # Aguarda um intervalo de tempo antes da próxima requisição (opcional)
 done
 
