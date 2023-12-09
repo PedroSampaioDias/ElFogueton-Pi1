7147981cf9745c02fecd613931af5c1cde7720b2
Lucas <lucas.fn4@gmail.com>
2023-12-09 15:52:04 -0300

    Mudança no script para calibrar o gps

--- calibrar.sh
+++ calibrar.sh
@@ -1,6 +1,8 @@
-bash iniciar_gravacao.sh 1
+bash iniciar_gravacao.sh -1
 bash parar_gravacao.sh
 bash apagar_dados.sh
-rm retorno_lancamento_1.csv
+
+arquivoRetorno=$(head -n 1 filepointer)
+rm $arquivoRetorno
 
 bash obter_quantidade_satelites.sh
